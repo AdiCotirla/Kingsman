88aed1fa3223c3c10c956906a8ebf3f2adc95ef4
AdiCotirla <adioprea53@gmail.com>
2022-02-10 22:20:53 +0200

    fixes

--- src/Components/Product/product.js
+++ src/Components/Product/product.js
@@ -4,7 +4,7 @@ import "../../Styling/product.css"
 const Product = (props) => {
   return <div className='product' key={props.id}>
       <img src={props.image} alt="product"></img>
-      <h2>{props.title}</h2>
+      <p>{props.title}</p>
       <h3>Price:{props.price}</h3>
       {props.onAddToCart ? <button onClick={props.onAddToCart}>Add to cart</button>:null}
   </div>;
